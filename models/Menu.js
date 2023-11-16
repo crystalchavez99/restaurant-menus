567d701cb6d36d26a884388f331948e0f80a0dd8
Crystal <crystal120599@gmail.com>
2023-11-16 09:43:31 -0800

    created model for menu and restaurant

--- models/Menu.js
+++ models/Menu.js
@@ -2,5 +2,7 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
-
+let Menu = sequelize.define("Menu", {
+title: Sequelize.STRING
+})
 module.exports = {Menu};
